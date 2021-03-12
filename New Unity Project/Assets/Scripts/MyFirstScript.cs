5217f5d55d23d946ce707d07e3ea3ff4b18a4694
Tushar Chaudhary <tusharchaudhary692@gmail.com>
2021-03-12 18:24:33 +0530

    Class 1 practice

---
+++ New Unity Project/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+   void Awake()
+    {
+        Debug.Log("My first Awake Function");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My first Start Function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("My first Update Function");
+        if(Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("F was pressed or Left click on Mouse");
+        }
+
+        if(Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("F is being pressed");
+        }
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("F has been released");
+        }
+    }
+    void LateUpdate()
+    {
+        Debug.Log("My first Late Update Function");
+    }
+}
---
+++ New Unity Project/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public Button button;
+    public string newScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    // Update is called once per frame
+    private void OnButtonClick()
+    {
+        SceneManager.LoadScene(newScene);
+    }
+}
